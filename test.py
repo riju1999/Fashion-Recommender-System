e2ab50540e1420a1539debbc0bcbc4c828fe008c
RijuDhara <104830371+riju1999@users.noreply.github.com>
2022-08-20 02:06:03 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,41 @@
+import tensorflow
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.layers import GlobalMaxPooling2D
+from tensorflow.keras.applications.resnet50 import ResNet50,preprocess_input
+import numpy as np
+from numpy.linalg import norm
+import pickle
+from sklearn.neighbors import NearestNeighbors
+import cv2
+
+
+feature_list = np.array(pickle.load(open("embeddings.pkl","rb")))
+#print(np.array(feature_list).shape)
+filenames = pickle.load(open("filenames.pkl","rb"))
+
+model = ResNet50(weights='imagenet',include_top=False,input_shape=(224,224,3))
+model.trainable = False
+
+model = tensorflow.keras.Sequential([
+    model,
+    GlobalMaxPooling2D()
+])
+
+img = image.load_img("sample/3.jpg",target_size=(224,224))
+img_array = image.img_to_array(img)
+expand_image_array = np.expand_dims(img_array,axis=0)
+preprocess_image = preprocess_input(expand_image_array)
+result = model.predict(preprocess_image).flatten()
+normalized_result = result/norm(result)
+
+neighbors = NearestNeighbors(n_neighbors=6,algorithm="brute",metric="euclidean")
+neighbors.fit(feature_list)
+
+distances,indices = neighbors.kneighbors([normalized_result])
+
+print(indices)
+
+for file in indices[0][1:6]:
+    temp_img = cv2.imread(filenames[file])
+    cv2.imshow("output",cv2.resize(temp_img,(512,512)))
+    cv2.waitKey(0)    
